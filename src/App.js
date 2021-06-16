9cca5be656e43d838dcaadac9f1830c095bbc319
Patrick <patrickvieiramota@gmail.com>
2021-06-15 23:39:27 -0300

    Criar estilos globais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <>
+      <GlobalStyle/>
       <Routes/>
     </>
   );
---
+++ src/styles/global.js
@@ -0,0 +1,42 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+    html, body, #root{
+        min-height: 100%;
+        max-width: 1440px;
+        margin: 0 auto;
+    }
+    body{
+        background: #E5E5E5;
+        -webkit-font-smoothing: antialiased !important;
+    }
+ 
+    body, input, button {
+        font-family: 'Roboto', sans-serif;
+        overflow-x: hidden;
+    }
+    a{
+        text-decoration: none;
+    }
+    button{
+        cursor: pointer;
+    }
+    :root{
+        --orange: #ED7839;
+        --orange-light: #FDF1EB;
+        --text: #5C5C5C;
+        --text-dark: #4E4E4E;
+        --gray: #F8F8F8;
+        --gray-dark: #EAF1F2;
+        --purple: #71478E;
+        --white: #FFFFFF;
+    }
+`
+
+export default GlobalStyle;
