9aebfe45509cb9df466b2c74bbb9aac02154832d
Patrick <patrickvieiramota@gmail.com>
2021-06-19 14:44:34 -0300

    Criar componente Resumo

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -293,6 +293,16 @@ export const Card = styled.div`
     border: 1px solid #ECECEC;
     box-sizing: border-box;
     border-radius: 3px;
+    margin-top: 20px;
+
+    cursor: ${props => props.clickable ? 'pointer': 'default'};
+
+    font-family: Quicksand;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 23px;
+    letter-spacing: 0.02em;
 `
 
 export const HeaderCard = styled.div`
@@ -339,4 +349,8 @@ export const FooterCard = styled.div`
     letter-spacing: 0.02em;
     color: var(--text);
     opacity: 0.6;
+`
+
+export const OrangeText = styled.strong`
+    color: var(--orange);
 `
